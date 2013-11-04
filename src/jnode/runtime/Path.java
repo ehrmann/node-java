6b9683ff8e796cf76207434eab33e12c6ad38f6a
David Ehrmann <ehrmann@gmail.com>
2013-11-03 20:24:43 -0800

    Fix support in Path for Windows-style path separators

--- src/jnode/runtime/Path.java
+++ src/jnode/runtime/Path.java
@@ -1,8 +1,29 @@
 package jnode.runtime;
 
 import java.io.File;
+import java.util.regex.Pattern;
 
 public class Path {
+		
+	protected final Pattern trailingSeparators;
+	protected final Pattern trailingFileAndSeparator;
+	protected final Pattern directory;
+	protected final String separator;
+	
+	public Path() {
+		this(File.separator);
+	}
+	
+	public Path(String separator) {
+		String qps = Pattern.quote(separator);
+		
+		this.trailingSeparators = Pattern.compile("(?<!^)" + qps + "*$");
+		this.trailingFileAndSeparator = Pattern.compile(qps + "+(?!.*" + qps + ".*).*$");
+		this.directory = Pattern.compile(".*" + qps + "(?!$)");
+		this.separator = separator;
+	}
+	
+	
 	public String resolve(String part, String rest) {
 		File path = new File(part);
 		File file2 = new File(path, rest);
@@ -11,14 +32,14 @@ public class Path {
 	
 	public String dirname(String file) {
 		// Trim trailing slashes (except a leading slash)
-		file = file.replaceFirst("(?<!^)/*$", "");
+		file = trailingSeparators.matcher(file).replaceFirst("");
 		
-		String dirname = file.replaceFirst("/+[^/]*$", "");
+		String dirname = trailingFileAndSeparator.matcher(file).replaceFirst("");
 		
 		if (dirname.length() == file.length()) {
 			dirname = ".";
 		} else if (dirname.isEmpty()){
-			dirname = "/";
+			dirname = this.separator;
 		}
 		
 		return dirname;
@@ -26,9 +47,9 @@ public class Path {
 	
 	public String basename(String file) {
 		// Trim trailing slashes (except a leading slash)
-		file = file.replaceFirst("(?<!^)/*$", "");
+		file = trailingSeparators.matcher(file).replaceFirst("");
 		
-		return file.replaceFirst(".*[/](?!$)", "");
+		return directory.matcher(file).replaceFirst("");
 	}
 	
 	public String basename(String file, String extension) {
--- test/jnode/runtime/PathTest.java
+++ test/jnode/runtime/PathTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class PathTest {
 
 	@Test
-	public void testDirname() {
-		Path path = new Path();
+	public void testUnixDirname() {
+		Path path = new Path("/");
 		
 		assertEquals(".", path.dirname("."));
 		assertEquals("/", path.dirname("/"));
@@ -23,8 +23,8 @@ public class PathTest {
 	}
 	
 	@Test
-	public void testBasename() {
-		Path path = new Path();
+	public void testUnixBasename() {
+		Path path = new Path("/");
 		
 		assertEquals(".", path.basename("."));
 		assertEquals(".", path.basename("./"));
@@ -44,4 +44,40 @@ public class PathTest {
 		assertEquals("bar", path.basename("foo/bar.html", ".html"));
 	}
 
+	public void testWinDirname() {
+		Path path = new Path("\\");
+		
+		assertEquals(".", path.dirname("."));
+		assertEquals("\\", path.dirname("\\"));
+		assertEquals("\\", path.dirname("\\foo"));
+		assertEquals("\\", path.dirname("\\foo\\"));
+		assertEquals("\\foo", path.dirname("\\foo\\bar\\\\"));
+		assertEquals("\\foo\\bar\\..", path.dirname("\\foo\\bar\\..\\\\..\\"));
+		assertEquals(".", path.dirname("foo"));
+		assertEquals(".", path.dirname(".\\foo"));
+		assertEquals("foo\\bar\\..", path.dirname("foo\\bar\\..\\\\..\\"));
+		assertEquals(".", path.dirname(".\\"));
+	}
+	
+	@Test
+	public void testWinBasename() {
+		Path path = new Path("\\");
+		
+		assertEquals(".", path.basename("."));
+		assertEquals(".", path.basename(".\\"));
+		assertEquals("\\", path.basename("\\"));
+		assertEquals(".", path.basename("\\.\\"));
+		assertEquals("..", path.basename("\\..\\"));
+		assertEquals("..", path.basename("\\..\\\\"));
+		assertEquals(".", path.basename("\\.\\\\"));
+		
+		assertEquals("foo", path.basename("foo"));
+		assertEquals("foo", path.basename("\\foo"));
+		assertEquals("bar", path.basename("foo\\bar"));
+		assertEquals("bar", path.basename("foo\\bar\\\\"));
+		
+		
+		assertEquals("..", path.basename("\\..\\\\\\", "\\\\"));
+		assertEquals("bar", path.basename("foo\\bar.html", ".html"));
+	}
 }
